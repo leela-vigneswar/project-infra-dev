8c43d4319af18db1c91210121ea7f677b4316750
vignesh <vigneshlevis@gmail.com>
2026-06-26 16:54:04 +0530

    terraform

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "URL"
+    source = "git::https://github.com/leela-vigneswar/terraform-aws-vpc.git"
     project = var.project
     environment = var.env
     is_peering_required = true
