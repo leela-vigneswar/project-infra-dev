20e81f7bac47301e656936aff1bd7ac77119fa88
vignesh <vigneshlevis@gmail.com>
2026-07-01 18:47:44 +0530

    terraform

---
+++ 70-acm/variables.tf
@@ -0,0 +1,20 @@
+variable "project" {
+    default = "myproject"
+  
+}
+
+variable "env" {
+    default = "dev"
+  
+}
+
+variable "zone_id" {
+    default = "Z1033902J5K5QBV5EVSW" #my hosted zone id
+}
+
+variable "domain_name" {
+    default = "leelavignesh.in" # my own domain
+}
+variable "app_version" {
+    default = "v3"
+}
