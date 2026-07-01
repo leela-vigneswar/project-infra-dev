039b0a5afec302ee404e869222b8fd67e156bbd5
vignesh <vigneshlevis@gmail.com>
2026-07-01 15:11:31 +0530

    terraform

---
+++ 40-databases/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+component=$1
+environment=$2
+dnf install ansible -y
+
+cd /home/ec2-user
+git clone https://github.com/leela-vigneswar/ansible-myproject-roles-tf.git
+
+cd ansible-myproject-roles-tf
+git pull
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
